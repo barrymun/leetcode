1e04389447bf126c210c2ba73c5718dbaadbb50b
Neil <barrymun@tcd.ie>
2022-10-27 09:52:59 +0100

    70 -> solved

--- top_interview/dynamic_programming/70_climbing_stairs/main.py
+++ top_interview/dynamic_programming/70_climbing_stairs/main.py
@@ -1,6 +1,13 @@
 def climb_stairs(n: int) -> int:
-    print(n)
+    res = 1  # will take at least 1 step
+    one, two = 1, 1  # both start from the first step
+    for i in range(0, n - 1):
+        res = one + two
+        two = one
+        one = res
+    return res
 
 
 if __name__ == '__main__':
-    climb_stairs(n=3)
+    for n in range(1, 6):
+        print(f'n={n}, res={climb_stairs(n=n)}')
