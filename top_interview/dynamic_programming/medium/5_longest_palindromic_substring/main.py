25f11547e435670e935e9241151d4e04a1b95b2f
Neil <barrymun@tcd.ie>
2022-10-27 19:33:19 +0100

    5 -> solved

--- top_interview/dynamic_programming/medium/5_longest_palindromic_substring/main.py
+++ top_interview/dynamic_programming/medium/5_longest_palindromic_substring/main.py
@@ -1,22 +1,27 @@
+def helper(s: str, l_index: int, r_index: int):
+    """
+    get the longest palindrome
+    :param s:
+    :param l_index:
+    :param r_index:
+    :return:
+    """
+    while 0 <= l_index and r_index < len(s) and s[l_index] == s[r_index]:
+        l_index -= 1
+        r_index += 1
+    return s[l_index + 1:r_index]
+
+
 def longest_palindrome(s: str) -> str:
     """
     :param s:
     :return:
     """
-    max_len = 0
     res = ""
-    if len(s) < 1 or len(s) > 1000:  # constraint
-        return res
-    for i in range(len(s), 0, -1):
-        # print(f'i={i}')
-        for j in range(0, i, 1):
-            # print(f'j={j}')
-            # print(f'res={s[j:i]}')
-            if s[j:i] == s[j:i][::-1]:
-                if len(s[j:i]) > max_len:
-                    # print(f'res={s[j:i]}')
-                    max_len = len(s[j:i])
-                    res = s[j:i]
+    for i in range(len(s)):
+        odd_case = helper(s, i, i)
+        even_case = helper(s, i, i + 1)
+        res = max(odd_case, even_case, res, key=len)
     return res
 
 
@@ -25,3 +30,6 @@ if __name__ == '__main__':
     print(longest_palindrome(s="babad"))
     print(longest_palindrome(s="cbbd"))
     print(longest_palindrome(s="cbbc"))
+    print(longest_palindrome(s="ac"))
+    print(longest_palindrome(
+        s="qkajbumzdzkiplmbcpnhbzweoevrvbptpozhtrfntszvnwbdahvkykmezrwruhvvslngruvwqebudtfxgpbmwefczwrecpqjegxkqknpobzkemmtruidulnxgntjxcmxtwmlxhzmbqfqylwvzjyojhfawwuupiipvxjiyxkqvsxbhgzzegfkdihizvjoxzrmeorikzsdyphbujaqmykrfblneqmwwxsoonzsgvligqxrrumspylfvquklbanjhkudlprwoycpxdsueokruoofyubirbhbyfuvgllijywuqmkcsfjttbnmelrylivkefllepgxnoeummujbaoyvryukyoumvuxezegpwgmwsupjuaarvbtbfmisrifjadqjypmzipvjysgakvjhfeaqwpsqijvqibshctuabwqqsjwotjopahoaptmxkwerkjkmwiodgblhtnhykzjuaoluoyokroxuvqtkpggfanzabgjejdfsgybhxbscubdpufywbxgutheskuhixasnksoayjngvhfoxxclykfobrwxjwgefarzczvptlfrgrtrjcemaeihpukhbeoezgvrwxgyhpkkfvmfvquwtswkdwqqgrgasopladdnteulqofmjhewpghkibbrewnhdllfppctgkfkoedoiwqocnpvfviochrokrgrzthrvyhqfsrzyyvqwkhuzsrkfaympcdodkwaojnghzytkhf"))
